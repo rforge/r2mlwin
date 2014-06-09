997ecd868668641533978bc1f5ab724a8669d486
cmjcharlton <cmjcharlton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-09 08:58:50 +0000

    Ensure that generated dummy variables aren't numeric.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/Untoggle.R
+++ R2MLwiN/R/Untoggle.R
@@ -1,19 +1,15 @@
 Untoggle <- function (categrv, name = NULL){
   ## this function will untoggle categorical variable into a few separate binary variables  
-  vars = unique(categrv)
-  N = length(vars)
-  rvs = sapply(1:N, function(x) as.integer(categrv == vars[x]))
-  if (is.factor(categrv)) {
-    colnames(rvs) = unique(as.character(categrv))
+  vars <- unique(categrv)
+  N <- length(vars)
+  rvs <- sapply(1:N, function(x) as.integer(categrv == vars[x]))
+  vals <- suppressWarnings(as.numeric(vars))
+  cnames <- as.character(vars)
+  if (!is.null(name)) {
+    cnames[!is.na(vals)] <- paste(name, cnames[!is.na(vals)], sep = "_")
+  } else {
+    cnames[!is.na(vals)] <- paste("v_", cnames[!is.na(vals)], sep = "_")
   }
-  else {
-    if (is.character(vars)) {
-      colnames(rvs) = vars
-    }
-    else {
-      if (!is.null(name) && is.numeric(vars))
-        colnames(rvs) = paste(name, vars, sep = "_")
-    }
-  }
+  colnames(rvs) <- cnames
   rvs
 }
