435745556f4d990f8797a74d024071f9e43cf8b2
zhengzheng <zhengzheng@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-12 21:10:59 +0000

    replace "1" by "Intercept" in clre (2nd attempt)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/runMLwiN.R
+++ R2MLwiN/R/runMLwiN.R
@@ -593,8 +593,8 @@ version:date:md5:filename:x64:trial
   if (is.null(clean.files)) clean.files=T
    
   clre=estoptions$clre
-  clre[2,] <- gsub("1", "Intercept", clre[2,])
-  clre[3,] <- gsub("1", "Intercept", clre[3,])
+  clre[2,] <- gsub("^1$", "Intercept", clre[2,])
+  clre[3,] <- gsub("^1$", "Intercept", clre[3,])
   
   smat=estoptions$smat
 
