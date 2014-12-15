f3afc3940814e00a90f4b839eee96cff998f9744
richardparker <richardparker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-15 16:15:52 +0000

    Roxygen2 added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@314 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/Untoggle.R
+++ R2MLwiN/R/Untoggle.R
@@ -1,3 +1,36 @@
+#' Converts a categorical variable into several separate binary variables
+#' 
+#' This function converts a vector (factor) of categorical character strings
+#' (integers) into several separate vectors of binary indicators.
+#' 
+#' @param categrv A vector (factor) of categorical character strings
+#' (integers).
+#' @param name A character string specifying a name of a prefix to be appended
+#' the categories when generating dummy variables. If \code{NULL}, \code{v__} is
+#' used as a prefix.
+#' 
+#' @return A matrix containing the generated dummy variables.
+#'
+#' @author Zhang, Z., Charlton, C.M.J., Parker, R.M.A., Leckie, G., and Browne,
+#' W.J. (2014) Centre for Multilevel Modelling, University of Bristol.
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' library(R2MLwiN)
+#' # NOTE: Assumes MLwiN path is C:/Program Files (x86)/MLwiN v2.30/
+#' # ...so please change relevant line if different
+#' # if using R2MLwiN via WINE, the path may look like 
+#' # options(MLwiN_path="/home/USERNAME/.wine/drive_c/Program Files (x86)/MLwiN v2.30/") 
+#' 
+#' # Example: tutorial
+#' data(tutorial)
+#' names(tutorial)
+#' tutorial = cbind(tutorial, Untoggle(tutorial[["school"]], "school"))
+#' names(tutorial)
+#' }
+#' 
+#' @export
 Untoggle <- function (categrv, name = NULL){
   ## this function will untoggle categorical variable into a few separate binary variables  
   vars <- unique(categrv)
