d4307cddbefbf141fc90cb19e8a2c880f94a6d7f
richardparker <richardparker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-15 15:02:38 +0000

    Roxygen2 added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@297 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/df2matrix.R
+++ R2MLwiN/R/df2matrix.R
@@ -1,3 +1,24 @@
+#' Translates a data.frame, formatted for use in multiple membership
+#' modelling in MLwiN, to a matrix. 
+#' 
+#' Translates a \code{\link[base]{data.frame}}, in a form usable by MLwiN for multiple membership models,
+#' into a \code{\link[base]{matrix}}. The data.frame needs to contain (a) columns with membership IDs
+#' (e.g. first row of which might be \code{2, 3, 5, 6, 0, 0}) and (b) columns containing weights
+#' (e.g. first row of which might be \code{0.25, 0.25, 0.25, 0.25, 0, 0}; in this example the first row of
+#' resulting matrix would be \code{0, 1, 1, 0, 1, 1}).
+#' 
+#' @param data A \code{\link[base]{data.frame}} object.
+#' @param idcols String vector of the identifier column names.
+#' @param weightcols String vector of the weight column names.
+#' 
+#' @return An adjacency matrix as returned by \code{\link[Matrix]{sparseMatrix}}.
+#' 
+#' @author Zhang, Z., Charlton, C.M.J., Parker, R.M.A., Leckie, G., and Browne,
+#' W.J. (2014) Centre for Multilevel Modelling, University of Bristol, U.K.
+#' 
+#' @seealso \code{\link{matrix2df}}
+#' 
+#' @export
 df2matrix <- function(data, idcols, weightcols) {
   if (!is.data.frame(data) && !is.matrix(data)) {
     stop("Invalid input data")
