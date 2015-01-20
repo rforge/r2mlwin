0855d4913af93e2910d4242c938e30d851dceeed
cmjcharlton <cmjcharlton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-20 11:52:32 +0000

    Set default MLwiN path based on current platform.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@344 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/init.R
+++ R2MLwiN/R/init.R
@@ -1,6 +1,14 @@
 .onAttach <- function(...) {
   if (is.null(getOption("MLwiN_path"))) {
-    options(MLwiN_path = "C:/Program Files (x86)/MLwiN v2.31/")
+    if (Sys.info()["sysname"] == "Linux") {
+      options(MLwiN_path = "/usr/bin/mlnscript")
+    }
+    if (Sys.info()["sysname"] == "Darwin") {
+      options(MLwiN_path = "/opt/mln/mlnscript")
+    }
+    if (Sys.info()["sysname"] == "Windows") {
+      options(MLwiN_path = "C:/Program Files (x86)/MLwiN v2.32/")
+    }
   }
   packageStartupMessage(paste0("The MLwiN_path option is currently set to ", getOption("MLwiN_path"), "\n", "To change this use: options(MLwiN_path=\"<path to MLwiN>\")\n"))
 } 
