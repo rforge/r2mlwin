62ba6366b076809f0949150dfa8f3bd5d746dc73
richardparker <richardparker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-21 09:56:07 +0000

    Add ref to Roxygen
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/BD.R
+++ R2MLwiN/R/BD.R
@@ -3,13 +3,18 @@
 #' An internal function, for use in \code{\link{sixway}}, which calculates the
 #' Brooks-Draper diagnostic, based on an unpublished paper by David Draper. It
 #' estimates the length of a Markov chain required to produce a mean estimate
-#' to k significant figures with a given accuracy (alpha).
+#' to k significant figures with a given accuracy (alpha). See Browne (2012)
+#' for further details.
 #' 
 #' @param est Numeric scalar for the mean of the distribution
 #' @param var Numeric scalar for the variance of the distribution
 #' @param rho The first lag (i.e. after zero) of the auto-correlation function (ACF) diagnostic
 #' @param k Integer scalar corresponding to the number of significant figures (defaults to \code{2})
 #' @param alpha Numeric scalar indicating the desired accuracy (defaults to \code{0.05})
+#' 
+#' @references
+#' Browne, W.J. (2012) MCMC Estimation in MLwiN, v2.26.
+#' Centre for Multilevel Modelling, University of Bristol.
 #' 
 #' @author Zhang, Z., Charlton, C.M.J., Parker, R.M.A., Leckie, G., and Browne,
 #' W.J. (2015) Centre for Multilevel Modelling, University of Bristol.
