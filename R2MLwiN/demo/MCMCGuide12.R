9a51a21142cbba76aa03850007e85039bc2fca34
richardparker <richardparker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-26 10:15:47 +0000

    Changed in light of modifications to variable names in sample datasets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@423 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/demo/MCMCGuide12.R
+++ R2MLwiN/demo/MCMCGuide12.R
@@ -33,34 +33,34 @@ bang$use4 <- relevel(bang$use4, 4)
 
 (mymodel <- runMLwiN(log(use4, cons) ~ 1, D = "Unordered Multinomial", estoptions = list(EstM = 1), data = bang))
 
-cat(paste("Pr(y = 1) =", round(exp(mymodel@FP["FP_Intercept_1"])/(1 + exp(mymodel@FP["FP_Intercept_1"]) + exp(mymodel@FP["FP_Intercept_2"]) + 
-  exp(mymodel@FP["FP_Intercept_3"])), 4), "\n"))
-cat(paste("Pr(y = 2) =", round(exp(mymodel@FP["FP_Intercept_2"])/(1 + exp(mymodel@FP["FP_Intercept_1"]) + exp(mymodel@FP["FP_Intercept_2"]) + 
-  exp(mymodel@FP["FP_Intercept_3"])), 4), "\n"))
-cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_3"])/(1 + exp(mymodel@FP["FP_Intercept_1"]) + exp(mymodel@FP["FP_Intercept_2"]) + 
-  exp(mymodel@FP["FP_Intercept_3"])), 4), "\n"))
+cat(paste("Pr(y = 1) =", round(exp(mymodel@FP["FP_Intercept_Sterilization"])/(1 + exp(mymodel@FP["FP_Intercept_Sterilization"]) + exp(mymodel@FP["FP_Intercept_Modern_reversible_method"]) + 
+  exp(mymodel@FP["FP_Intercept_Traditional_method"])), 4), "\n"))
+cat(paste("Pr(y = 2) =", round(exp(mymodel@FP["FP_Intercept_Modern_reversible_method"])/(1 + exp(mymodel@FP["FP_Intercept_Sterilization"]) + exp(mymodel@FP["FP_Intercept_Modern_reversible_method"]) + 
+  exp(mymodel@FP["FP_Intercept_Traditional_method"])), 4), "\n"))
+cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_Traditional_method"])/(1 + exp(mymodel@FP["FP_Intercept_Sterilization"]) + exp(mymodel@FP["FP_Intercept_Modern_reversible_method"]) + 
+  exp(mymodel@FP["FP_Intercept_Traditional_method"])), 4), "\n"))
 
 # 12.2 Adding predictor variables . . . . . . . . . . . . . . . . . . . .173
 
 (mymodel <- runMLwiN(log(use4, cons) ~ 1 + lc, D = "Unordered Multinomial", estoptions = list(EstM = 1), data = bang))
 
-cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_3"])/(1 + exp(mymodel@FP["FP_Intercept_1"]) + exp(mymodel@FP["FP_Intercept_2"]) + 
-  exp(mymodel@FP["FP_Intercept_3"])), 4), "\n"))
-cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_3"] + mymodel@FP["FP_lcTwo_children_3"])/(1 + exp(mymodel@FP["FP_Intercept_1"] + 
-  mymodel@FP["FP_lcTwo_children_1"]) + exp(mymodel@FP["FP_Intercept_2"] + mymodel@FP["FP_lcTwo_children_2"]) + exp(mymodel@FP["FP_Intercept_3"] + 
-  mymodel@FP["FP_lcTwo_children_3"])), 4), "\n"))
+cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_Traditional_method"])/(1 + exp(mymodel@FP["FP_Intercept_Sterilization"]) + exp(mymodel@FP["FP_Intercept_Modern_reversible_method"]) + 
+  exp(mymodel@FP["FP_Intercept_Traditional_method"])), 4), "\n"))
+cat(paste("Pr(y = 3) =", round(exp(mymodel@FP["FP_Intercept_Traditional_method"] + mymodel@FP["FP_lcTwo_children_Traditional_method"])/(1 + exp(mymodel@FP["FP_Intercept_Sterilization"] + 
+  mymodel@FP["FP_lcTwo_children_Sterilization"]) + exp(mymodel@FP["FP_Intercept_Modern_reversible_method"] + mymodel@FP["FP_lcTwo_children_Modern_reversible_method"]) + exp(mymodel@FP["FP_Intercept_Traditional_method"] + 
+  mymodel@FP["FP_lcTwo_children_Traditional_method"])), 4), "\n"))
 
 # 12.3 Interval estimates for conditional probabilities . . . . . . . . .175
 
 chains <- mymodel@chains
-pred1 <- exp(chains[, "FP_Intercept_3"])/(1 + exp(chains[, "FP_Intercept_1"]) + exp(chains[, "FP_Intercept_2"]) + 
-  exp(chains[, "FP_Intercept_3"]))
+pred1 <- exp(chains[, "FP_Intercept_Traditional_method"])/(1 + exp(chains[, "FP_Intercept_Sterilization"]) + exp(chains[, "FP_Intercept_Modern_reversible_method"]) + 
+  exp(chains[, "FP_Intercept_Traditional_method"]))
 summary(pred1)
 sixway(pred1, "prob1")
 
-pred2 <- exp(chains[, "FP_Intercept_3"] + chains[, "FP_lcTwo_children_3"])/(1 + exp(chains[, "FP_Intercept_1"] + chains[, 
-  "FP_lcTwo_children_1"]) + exp(chains[, "FP_Intercept_2"] + chains[, "FP_lcTwo_children_2"]) + exp(chains[, "FP_Intercept_3"] + 
-  chains[, "FP_lcTwo_children_3"]))
+pred2 <- exp(chains[, "FP_Intercept_Traditional_method"] + chains[, "FP_lcTwo_children_Traditional_method"])/(1 + exp(chains[, "FP_Intercept_Sterilization"] + chains[, 
+  "FP_lcTwo_children_Sterilization"]) + exp(chains[, "FP_Intercept_Modern_reversible_method"] + chains[, "FP_lcTwo_children_Modern_reversible_method"]) + exp(chains[, "FP_Intercept_Traditional_method"] + 
+  chains[, "FP_lcTwo_children_Traditional_method"]))
 summary(pred2)
 sixway(pred2, "prob1")
 
@@ -73,7 +73,7 @@ sixway(pred2, "prob1")
 ## Uses MCMC
 (mymodel <- runMLwiN(log(use4, cons) ~ 1 + lc + (district | 1), D = "Unordered Multinomial", estoptions = list(EstM = 1, 
   nonlinear = c(1, 2)), data = bang))
-sixway(mymodel@chains[, "RP2_var_Intercept_1", drop = FALSE], "sigma2v0")
+sixway(mymodel@chains[, "RP2_var_Intercept_Sterilization", drop = FALSE], "sigma2v0")
 
 RP3.cons <- matrix(, 3, 3)
 RP3.cons[upper.tri(RP3.cons, diag = TRUE)] <- mymodel@RP[1:6]
