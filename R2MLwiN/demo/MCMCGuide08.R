f6d99c2d80b53ea261f11ece2fa17e6a7209f1ba
cmjcharlton <cmjcharlton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-27 17:00:26 +0000

    Update example to run simulations in parallel.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@467 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/demo/MCMCGuide08.R
+++ R2MLwiN/demo/MCMCGuide08.R
@@ -27,13 +27,15 @@ while (!file.access(mlwin, mode = 1) == 0) {
 }
 options(MLwiN_path = mlwin)
 
+if (!require(doParallel)) install.packages("doParallel")
+library(doParallel)
+
 # User's input if necessary
 
 # 8.3 Generating simulated datasets based on true values . . . . . . . . 102
 
 # 8.4 Fitting the model to the simulated datasets . . . . . . . . . . . .106
 
-set.seed(1)
 pupil <- 1:108
 school <- c(rep(1, 18), rep(2, 18), rep(3, 18), rep(4, 18), rep(5, 18), rep(6, 18))
 cons <- rep(1, 108)
@@ -43,41 +45,69 @@ IGLS_array <- MCMC_array <- array(, c(3, 2, ns))
 MCMC_median <- data.frame(RP2_var_Intercept = rep(0, ns), RP1_var_Intercept = rep(0, ns))
 CounterMCMC <- rep(0, 3)
 Actual <- c(30, 10, 40)
-for (i in 1:ns) {
-  u_short <- rnorm(6, 0, sqrt(Actual[2]))
-  u <- rep(u_short, each = 18, len = 108)
-  e <- rnorm(108, 0, sqrt(Actual[3]))
-  resp <- Actual[1] * cons + u + e
-  indata <- data.frame(cbind(pupil, school, cons, resp))
-  simModelIGLS <- runMLwiN(resp ~ 1 + (1 | school) + (1 | pupil), data = indata)
-  IGLS_array[, , i] <- cbind(coef(simModelIGLS), diag(vcov(simModelIGLS)))
-  simModelMCMC <- runMLwiN(resp ~ 1 + (1 | school) + (1 | pupil), estoptions = list(EstM = 1), data = indata)
-  MCMC_array[, , i] <- cbind(coef(simModelMCMC), diag(vcov(simModelMCMC)))
-  MCMC_median[i, ] <- c(median(simModelMCMC@chains[, "RP2_var_Intercept"]), median(simModelMCMC@chains[, "RP1_var_Intercept"]))
-  if (Actual[1] > quantile(simModelMCMC@chains[, "FP_Intercept"], 0.025) & Actual[1] < quantile(simModelMCMC@chains[, 
-    "FP_Intercept"], 0.975)) {
+
+simu <- function(i) {
+    u_short <- rnorm(6, 0, sqrt(Actual[2]))
+    u <- rep(u_short, each = 18, len = 108)
+    e <- rnorm(108, 0, sqrt(Actual[3]))
+    resp <- Actual[1] * cons + u + e
+    indata <- data.frame(cbind(pupil, school, cons, resp))
+    simModelIGLS <- runMLwiN(resp ~ 1 + (1 | school) + (1 | pupil), data = indata)
+    simModelMCMC <- runMLwiN(resp ~ 1 + (1 | school) + (1 | pupil), estoptions = list(EstM = 1),
+    data = indata)
+
+    quantile25 <- c(quantile(simModelMCMC@chains[, "FP_Intercept"], 0.025),
+                   quantile(simModelMCMC@chains[, "RP2_var_Intercept"], 0.025),
+                   quantile(simModelMCMC@chains[, "RP1_var_Intercept"], 0.025))
+    quantile975 <- c(quantile(simModelMCMC@chains[, "FP_Intercept"], 0.975),
+                   quantile(simModelMCMC@chains[, "RP2_var_Intercept"], 0.975),
+                   quantile(simModelMCMC@chains[, "RP1_var_Intercept"], 0.975))
+
+    list(MCMCarray=cbind(coef(simModelMCMC), diag(vcov(simModelMCMC))),
+         MCMCmed=c(median(simModelMCMC@chains[, "RP2_var_Intercept"]),
+                   median(simModelMCMC@chains[, "RP1_var_Intercept"])),
+         IGLSarray=cbind(coef(simModelIGLS), diag(vcov(simModelIGLS))),
+         quantiles=cbind(quantile25, quantile975))
+}
+  
+RNGkind("L'Ecuyer-CMRG")
+cl <- makeCluster(detectCores(logical = FALSE))
+registerDoParallel(cl)
+set.seed(1)
+r <- foreach(i=1:ns, .packages="R2MLwiN") %dopar% {
+  simu(i)
+}
+
+stopCluster(cl)
+
+for(i in 1:ns){
+  if (Actual[1] > r[[i]]$quantiles[1,1] & Actual[1] < r[[i]]$quantiles[1,2]) {
     CounterMCMC[1] <- CounterMCMC[1] + 1
   }
-  if (Actual[2] > quantile(simModelMCMC@chains[, "RP2_var_Intercept"], 0.025) & Actual[2] < quantile(simModelMCMC@chains[, 
-    "RP2_var_Intercept"], 0.975)) {
+  if (Actual[2] > r[[i]]$quantiles[2,1] & Actual[2] < r[[i]]$quantiles[2,2]) {
     CounterMCMC[2] <- CounterMCMC[2] + 1
   }
-  if (Actual[3] > quantile(simModelMCMC@chains[, "RP1_var_Intercept"], 0.025) & Actual[3] < quantile(simModelMCMC@chains[, 
-    "RP1_var_Intercept"], 0.975)) {
+  if (Actual[3] > r[[i]]$quantiles[3,1] & Actual[3] < r[[i]]$quantiles[3,2]) {
     CounterMCMC[3] <- CounterMCMC[3] + 1
   }
+  MCMC_array[, , i] <- r[[i]]$MCMCarray
+  MCMC_median[i, ] <- r[[i]]$MCMCmed
+  IGLS_array[, , i] <- r[[i]]$IGLSarray
 }
 
 aa <- sapply(1:ns, function(x) na.omit(stack(as.data.frame(IGLS_array[, , x])))$values)
 counterIGLS <- rep(0, 3)
 for (i in 1:ns) {
-  if (Actual[1] > aa[1, i] - 1.96 * sqrt(aa[4, i]) && Actual[1] < aa[1, i] + 1.96 * sqrt(aa[4, i])) {
+  if (Actual[1] > aa[1, i] - 1.96 * sqrt(aa[4, i]) && Actual[1] < aa[1, i] + 1.96 * sqrt(aa[4, i]))
+  {
     counterIGLS[1] <- counterIGLS[1] + 1
   }
-  if (Actual[2] > aa[2, i] - 1.96 * sqrt(aa[5, i]) && Actual[2] < aa[2, i] + 1.96 * sqrt(aa[5, i])) {
+  if (Actual[2] > aa[2, i] - 1.96 * sqrt(aa[5, i]) && Actual[2] < aa[2, i] + 1.96 * sqrt(aa[5, i]))
+  {
     counterIGLS[2] <- counterIGLS[2] + 1
   }
-  if (Actual[3] > aa[3, i] - 1.96 * sqrt(aa[6, i]) && Actual[3] < aa[3, i] + 1.96 * sqrt(aa[6, i])) {
+  if (Actual[3] > aa[3, i] - 1.96 * sqrt(aa[6, i]) && Actual[3] < aa[3, i] + 1.96 * sqrt(aa[6, i]))
+  {
     counterIGLS[3] <- counterIGLS[3] + 1
   }
 }
@@ -102,7 +132,8 @@ MCMC_results
 
 # Investigating median estimates with Gamma(epsilon, epsilon) priors
 
-Mean_across_simus <- round(c(mean(MCMC_median$RP2_var_Intercept), mean(MCMC_median$RP1_var_Intercept)), 2)
+Mean_across_simus <- round(c(mean(MCMC_median$RP2_var_Intercept),
+mean(MCMC_median$RP1_var_Intercept)), 2)
 Actual <- tail(Actual, -1)
 Percent_bias <- round(-100 * (1 - Mean_across_simus/Actual), 2)
 Percent_interval_coverage <- tail(Percent_interval_coverage, -1)
