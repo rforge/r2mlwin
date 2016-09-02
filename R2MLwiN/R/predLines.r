9a46a23e3d9282a77c6e191d28d77ab7340f1177
richardparker <richardparker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-02 11:24:17 +0000

    Added note on high memory usage re: 32/64-bit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@603 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/predLines.r
+++ R2MLwiN/R/predLines.r
@@ -1,7 +1,8 @@
 #' Draws predicted lines using a fitted model object
 #'
 #' This function draws predicted lines against an explanatory variable for
-#' selected groups at a higher (>=2) level.
+#' selected groups at a higher (>=2) level. Note that it uses a lot of contiguous memory, and so
+#' we recommend running via 64-bit version R to mititage against any potential problems.
 #'
 #' @param object Either an \code{\link{mlwinfitIGLS-class}} or \code{\link{mlwinfitMCMC-class}}
 #' object.
