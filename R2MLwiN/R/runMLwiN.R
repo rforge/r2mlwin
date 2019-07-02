fdf84667ebc2869427be76ac06ea7f48abf74a53
cmjcharlton <cmjcharlton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-02 08:54:28 +0000

    Add md5 for Raspbian-Buster.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mlwin@692 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R2MLwiN/R/runMLwiN.R
+++ R2MLwiN/R/runMLwiN.R
@@ -1037,6 +1037,7 @@ version:date:md5:filename:x64:trial:platform
 3.04:Jun 2019:009dc3d20263e219172d521182810893:mlnscript:TRUE:FALSE:lin
 3.04:Jun 2019:79d6a1100aae453fd2a1bd8c87c2a621:mlnscript:TRUE:FALSE:lin
 3.04:Jun 2019:6535de1f09f7e548dd78b58627d65a08:mlnscript:FALSE:FALSE:lin
+3.04:Jun 2019:d4ca11bf78b199a516f3622638474ac2:mlnscript:FALSE:FALSE:lin
 3.04:Jun 2019:64ea7da78bd94da1890f0ea1570e2402:mlnscript:TRUE:FALSE:mac
 3.04:Jun 2019:3f36bb6cd0008fd37c8ff96d5b1e1fc3:mlnscript:TRUE:FALSE:bsd
 3.04:Jun 2019:b2258c495213e0bb99550e1e91171bfb:mlnscript:TRUE:FALSE:bsd
